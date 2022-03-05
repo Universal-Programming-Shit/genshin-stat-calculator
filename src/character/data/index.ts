b7104c3199707ec70d9872d7ab044a0cecf797b5
Rhal95 <rhal9858@gmail.com>
2022-03-05 13:44:29 +0100

    Fix typos

--- src/character/data/index.ts
+++ src/character/data/index.ts
@@ -4,18 +4,18 @@ import Amber from "./Amber"
 import AratakiItto from "./AratakiItto"
 import Barbara from "./Barbara"
 import Beidou from "./Beidou"
-import Bennette from "./Bennett"
+import Bennett from "./Bennett"
 import Chongyun from "./Chongyun"
 import Diluc from "./Diluc"
 import Diona from "./Diona"
 import Eula from "./Eula"
 import Fischl from "./Fischl"
 import Ganyu from "./Ganyu"
-import Gorou from ".Gorou"
+import Gorou from "./Gorou"
 import HuTao from "./HuTao"
 import Jean from "./Jean"
 import KaedeharaKazuha from "./KaedeharaKazuha"
-import Kaeya from "./Keaya"
+import Kaeya from "./Kaeya"
 import Mona from "./Mona"
 import Yanfei from "./Yanfei"
 import {Character} from "../../types/character";
@@ -24,13 +24,13 @@ export default [
   Albedo,
   Aloy,
   Amber,
-  AratakiItto, 
-  Barbara, 
-  Beidou, 
-  Bennett, 
-  Chongyun, 
-  Diluc, 
-  Diona, 
+  AratakiItto,
+  Barbara,
+  Beidou,
+  Bennett,
+  Chongyun,
+  Diluc,
+  Diona,
   Eula,
   Fischl,
   Ganyu,
