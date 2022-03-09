aa28dbae7c5928ffc9356e6451685ae243b44c41
Rhal95 <rhal9858@gmail.com>
2022-03-09 15:26:04 +0100

    update character selection

--- src/character/CharacterStore.ts
+++ src/character/CharacterStore.ts
@@ -1,28 +1,28 @@
 import {defineStore} from "pinia";
 import data from "./data";
-import clamp from "../util/clamp";
 import getElem from "../util/getElem";
 import {CharacterStats} from "../types/characterStats";
 import {Character} from "../types/character";
-import { useStorage } from '@vueuse/core'
+import {useStorage} from "@vueuse/core";
 
 export const useCharacterStore = defineStore("character", {
   state: () => useStorage("character", {
     selectedCharacterIndex: 0,
-    activeStatsIndex: 0,
+    selectedStatsIndex: 0,
   }),
   getters: {
     name: (): string => this.selectedCharacter.name,
-    selectedCharacter: (state): Character => getElem(data, state.selectedCharacterIndex),
-    selectedStats: (state): CharacterStats => getElem(state.selectedCharacter.stats, state.activeStatsIndex),
-  },
-  actions: {
-    selectCharacter(i: number) {
-      this.selectedCharacterIndex = clamp(i, 0, data.length-1);
-      this.activeStatsIndex = clamp(this.activeStatsIndex, 0, this.selectedCharacter.stats.length-1);
+    selectedCharacter: (state): Character => getElem(data, state.selectedCharacterIndex) ?? {
+      name: "",
+      stats: [],
     },
-    selectStats(i: number) {
-      this.activeStatsIndex = clamp(i, 0, this.selectedCharacter.stats.length-1);
+    selectedStats: (state): CharacterStats => getElem(state.selectedCharacter.stats, state.selectedStatsIndex) ?? {
+      baseDef: 0,
+      ascentionStatValue: 0,
+      baseAtk: 0,
+      baseHp: 0,
+      ascensions: 0,
+      level: 0,
     },
   },
 });
